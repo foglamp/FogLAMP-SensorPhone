1825337eb5765226bf8904f0147d411ea4ee749d
Mark Riddoch <mark@scaledb.com>
2018-01-08 11:04:31 +0100

    Update cmds.sql

--- cmds.sql
+++ cmds.sql
@@ -5,7 +5,7 @@ INSERT INTO foglamp.scheduled_processes ( name, script ) VALUES ( 'SensorApp', '
 
 --- Add the schedule to start the service at system startup
 INSERT INTO foglamp.schedules ( id, schedule_name, process_name, schedule_type,schedule_interval, exclusive )
-     VALUES ( '543a59ce-a9ca-11e7-abc4-cec278b6b11a', 'device', 'SensorApp', 1, '0:0', true );
+     VALUES ( '543a59ce-a9ca-11e7-abc4-cec278b6b11a', 'SensorPhone', 'SensorApp', 1, '0:0', true );
 
 --- Insert the config needed to load the plugin
 INSERT INTO foglamp.configuration ( key, description, value )
